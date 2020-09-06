78b6d4d4dd063d320ab449073d71071e677bb65e
guinter_trisch <guintert@dbserver.com.br>
2020-09-06 18:11:09 -0300

    adicionado segundo teste

--- src/test/java/CadastroPessoaTest.java
+++ src/test/java/CadastroPessoaTest.java
@@ -1,13 +1,21 @@
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CadastroPessoaTest {
-
 
     @ParameterizedTest
     @ValueSource(strings = {"pessoa fisica", "pessoa juridica"})
     public void deveCadastrarPessoa(String tipoPessoa) {
         System.out.println(tipoPessoa);
     }
+
+    @ParameterizedTest
+    @CsvSource({"Guinter,pessoa fisica",
+            "Maria,pessoa juridica"})
+    public void deveCadastrarPessoaETipo(String nome, String tipoPessoa) {
+        System.out.println(nome + " - " + tipoPessoa
+        );
+    }
+
 }
